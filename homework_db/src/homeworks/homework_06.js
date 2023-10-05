68bc4be6be75e5f06985d242a58bae35e4a759aa
Natalia Bodnaryuk <132370042+nata-bo@users.noreply.github.com>
2023-10-05 14:45:17 +0200

    add homework_06

---
+++ homework_db/src/homeworks/homework_06.js
@@ -0,0 +1,42 @@
+// Задача 1. Разблокировать юзеров из France и Germany с положительным балансом
+
+db.users.updateMany(
+    {country:{$in:['France','Germany']}, balance:{$gt:0}},
+    { $unset: { is_blocked: null } }
+)
+
+// Задача 2. Вывести названия двух произвольных треков
+
+db.tracks.aggregate([
+    { $sample: { size: 2 } },
+     {$project: {title:1, _id: 0}}
+])
+
+// Задача 3. Вывести юзеров, у которых есть треки
+
+db.users.aggregate([
+    {
+        $lookup: {
+            from: 'tracks',
+            localField: '_id',
+            foreignField: 'author_id',
+            as: 'tracks'
+        }
+    },
+     { $match: { tracks:{ $ne: [] }} }
+])
+
+// Задача 4. Используя метод aggregate(), вывести ко-во юзеров без треков
+
+db.users.aggregate([
+ {
+        $lookup: {
+            from: 'tracks',
+            localField: '_id',
+            foreignField: 'author_id',
+            as: 'tracks'
+        }
+    },
+     { $match: { tracks: [] } },
+     {$count: 'users_count'}
+])
